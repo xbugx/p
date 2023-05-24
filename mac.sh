d2d00bac67024083913e097cba1aed077a9fcf73
漏洞 <100210211@qq.com>
2023-05-24 11:44:55 +0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,7 +1,10 @@
-curl -O ~/Download/ https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.darwin-amd64.tar.gz
-cd ~/Download
+curl -o ~/Download/node_exporter-1.4.0.darwin-amd64.tar.gz https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.darwin-amd64.tar.gz
+cd ~/Downloads
 tar -xzf node_exporter-1.4.0.darwin-amd64.tar.gz
 mkdir -p /usr/local/bin
 cp -n node_exporter-1.4.0.darwin-amd64/node_exporter /usr/local/bin/
 
-curl -O  ~/Download/ https://raw.githubusercontent.com/prometheus/node_exporter/master/examples/launchctl/io.prometheus.node_exporter.plist -O /Library/LaunchDaemons/io.prometheus.node_exporter.plist
+curl -o /Library/LaunchDaemons/io.prometheus.node_exporter.plist https://raw.githubusercontent.com/prometheus/node_exporter/master/examples/launchctl/io.prometheus.node_exporter.plist -O /Library/LaunchDaemons/io.prometheus.node_exporter.plist
+chown root:wheel /Library/LaunchDaemons/io.prometheus.node_exporter.plist
+
+launchctl bootstrap system/ /Library/LaunchDaemons/io.prometheus.node_exporter.plist
