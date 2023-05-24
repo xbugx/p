51b7937602d979489b76ce0a4572a681c50ad595
漏洞 <100210211@qq.com>
2023-05-24 11:52:29 +0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,4 +1,4 @@
-curl -o ~/Download/node_exporter-1.4.0.darwin-amd64.tar.gz https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.darwin-amd64.tar.gz
+curl -o ~/Downloads/node_exporter-1.4.0.darwin-amd64.tar.gz https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.darwin-amd64.tar.gz
 cd ~/Downloads
 tar -xzf node_exporter-1.4.0.darwin-amd64.tar.gz
 mkdir -p /usr/local/bin
