026e6cb19c9acf98be6bbab5f1ce54b3c4b9c413
漏洞 <100210211@qq.com>
2023-05-24 11:49:01 +0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -4,7 +4,7 @@ tar -xzf node_exporter-1.4.0.darwin-amd64.tar.gz
 mkdir -p /usr/local/bin
 cp -n node_exporter-1.4.0.darwin-amd64/node_exporter /usr/local/bin/
 
-curl -o /Library/LaunchDaemons/io.prometheus.node_exporter.plist https://raw.githubusercontent.com/prometheus/node_exporter/master/examples/launchctl/io.prometheus.node_exporter.plist -O /Library/LaunchDaemons/io.prometheus.node_exporter.plist
+curl -o /Library/LaunchDaemons/io.prometheus.node_exporter.plist https://raw.githubusercontent.com/prometheus/node_exporter/master/examples/launchctl/io.prometheus.node_exporter.plist
 chown root:wheel /Library/LaunchDaemons/io.prometheus.node_exporter.plist
 
 launchctl bootstrap system/ /Library/LaunchDaemons/io.prometheus.node_exporter.plist
